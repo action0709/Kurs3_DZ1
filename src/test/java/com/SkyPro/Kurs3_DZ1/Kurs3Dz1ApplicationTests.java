6bdc2275a00fdf3bd83379c546683d0e02d4058f
action0709 <action0709@mail.ru>
2023-08-07 19:21:32 +0400

    1

---
+++ src/test/java/com/SkyPro/Kurs3_DZ1/Kurs3Dz1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkyPro.Kurs3_DZ1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs3Dz1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
